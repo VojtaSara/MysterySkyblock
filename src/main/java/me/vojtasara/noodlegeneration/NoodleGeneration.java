7875c7e834400d1085420ffa464537a06db3b373
VojtaSara <vojta.sara@gmail.com>
2022-02-11 10:49:16 +0100

    First event handler

--- src/main/java/me/vojtasara/noodlegeneration/NoodleGeneration.java
+++ src/main/java/me/vojtasara/noodlegeneration/NoodleGeneration.java
@@ -1,20 +1,22 @@
 package me.vojtasara.noodlegeneration;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class NoodleGeneration extends JavaPlugin {
+public final class NoodleGeneration extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         System.out.println("PLUGIN STARTED SUCCESFULLY");
 
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-        System.out.println("PLUGIN ended SUCCESFULLY");
-
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+        event.setJoinMessage("HELLO PLAYER");
     }
 }
